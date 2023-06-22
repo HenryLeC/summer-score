2dfed88a99baf72533b1fad95de2cfe01f88a694
Henry LeCompte <henry@lunar-glass.com>
2023-06-22 08:23:20 -0400

    Add white outline to scoreboard

--- src/components/TeamScorePaper.tsx
+++ src/components/TeamScorePaper.tsx
@@ -10,10 +10,24 @@ function TeamScorePaper({ teamName, teamColor, score }: TeamScorePaperProps) {
   return (
     <Paper elevation={3} style={{ backgroundColor: 'black', color: 'white' }}>
       <center>
-        <Typography variant='h4' component='div' color={teamColor}>
+        <Typography
+          variant='h4'
+          component='div'
+          color={teamColor}
+          style={{
+            WebkitTextStroke: '2px white',
+          }}
+        >
           {teamName}
         </Typography>
-        <Typography variant='h1' component='div' color={teamColor}>
+        <Typography
+          variant='h1'
+          component='div'
+          color={teamColor}
+          style={{
+            WebkitTextStroke: '2px white',
+          }}
+        >
           {score}
         </Typography>
       </center>
